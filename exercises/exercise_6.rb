5a3c2b9a93404aeb80a7d935414bdd3ae709a9ec
Adam <adam.dipinto@gmail.com>
2016-12-07 00:31:15 +0000

    Completed exercise5. Added employees to the stores.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,15 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create([
+  {first_name: "Dave", last_name: "Bob", hourly_rate: 12},
+  {first_name: "Mario", last_name: "Luigi", hourly_rate: 30},
+  {first_name: "Luigi", last_name: "Luigi", hourly_rate: 29}
+  ])
+@store2.employees.create([
+  {first_name: "Kanye", last_name: "West", hourly_rate: 4000},
+  {first_name: "Biggie", last_name: "Smalls", hourly_rate: 300}
+  ])
+
+pp @store1.employees
+pp @store2.employees
